d47daa43cfb8a83fd814473c78adfcd6fd21cc16
Joshua Goulden <joshtgoulden2000@gmail.com>
2026-05-05 16:04:57 -0300

    Reorganize Vite config for plugins and build options

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/game-dev-portfolio/',
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
   },
 });
